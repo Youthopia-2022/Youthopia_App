f4eb9a6c8a387eda8f792d475bbf680a0dc924ef
TheDarkLord100 <miracleyt404@gmail.com>
2022-10-11 14:16:10 +0530

    Added login Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youthopia_2022_app/screens/login_screen.dart';
 import 'package:youthopia_2022_app/screens/nav_bar_screen.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: NavBarScreen(),
+      home: Login(),
     );
   }
 }
