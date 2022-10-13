e7a865b3c0037f6e6e6b22a37509495327346a3e
TheDarkLord100 <miracleyt404@gmail.com>
2022-10-13 13:26:40 +0530

    Initialised supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:youthopia_2022_app/screens/login_screen.dart';
+import 'package:supabase/supabase.dart';
 
-void main() {
+
+Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  const supabaseUrl = 'https://bwqibqbxbirzbyjqovco.supabase.co';
+  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3cWlicWJ4YmlyemJ5anFvdmNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU2NDMxNTcsImV4cCI6MTk4MTIxOTE1N30.7TLUvzsw1-YI-G02oDJbsHHH7U5d1RS25-b-mLiXL6M';
+
+  final client = SupabaseClient(supabaseUrl, supabaseKey);
+
+
   runApp(
 
       //! Uncomment to enable Device Preview
