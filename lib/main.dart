f5b251d66dbc84c4374fbdd0c6910f8201c66e37
TheDarkLord100 <miracleyt404@gmail.com>
2022-10-15 23:11:13 +0530

    Minor change

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:youthopia_2022_app/screens/login_screen.dart';
-import 'package:supabase/supabase.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
 Future<void> main() async{
   WidgetsFlutterBinding.ensureInitialized();
 
-  const supabaseUrl = 'https://bwqibqbxbirzbyjqovco.supabase.co';
-  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ3cWlicWJ4YmlyemJ5anFvdmNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjU2NDMxNTcsImV4cCI6MTk4MTIxOTE1N30.7TLUvzsw1-YI-G02oDJbsHHH7U5d1RS25-b-mLiXL6M';
-
-  final client = SupabaseClient(supabaseUrl, supabaseKey);
   await dotenv.load(fileName: ".env");
 
   runApp(
-
       //! Uncomment to enable Device Preview
 /*     DevicePreview(
       enabled: !kReleaseMode,
