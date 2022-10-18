fff7215f0ec9c234c7fd91c1a7e235dfb7d62d6e
TheDarkLord100 <miracleyt404@gmail.com>
2022-10-18 14:52:11 +0530

    Added assets and changed Signup UI

---
+++ lib/constants/color_theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColourTheme{
+
+  static Color pink = const Color(0xFFff6da0);
+
+  static LinearGradient primaryGradient = const LinearGradient(
+      colors: <Color>[
+        Color(0xFFff6da0),
+        Color(0xFF1bbee9)
+      ]);
+}
