4587334c717b5e8c5da32d675e5c01930a610328
TheDarkLord100 <miracleyt404@gmail.com>
2022-10-21 13:22:54 +0530

    Changed Navbar UI

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -8,8 +8,6 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-
-
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
