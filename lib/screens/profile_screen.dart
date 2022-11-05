b96edaca58af172c59a29d614282c7f0df9a1fc0
Deepraj Baiyda <deeprajbaidya@gmail.com>
2022-11-06 01:45:16 +0530

    fix: Profile Screen a/c to latest UI closes #82

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:youthopia_2022_app/screens/login_screen.dart';
 import 'package:youthopia_2022_app/services/users.dart';
@@ -59,155 +60,176 @@ class _ProfileScreenState extends State<ProfileScreen> {
       body: (_name == null)
           ? const Center(child: CircularProgressIndicator())
           : SingleChildScrollView(
-              child: SizedBox(
-                height: 600,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      height: 30,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Center(
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: AssetImage(_genderImage))),
                     ),
-                    Center(
-                      child: Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                                fit: BoxFit.cover,
-                                image: AssetImage(_genderImage))),
-                      ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Center(
+                    child: Text(
+                      _name!,
+                      style: TextStyle(fontSize: 22, color: ColourTheme.white),
                     ),
-                    const SizedBox(
-                      height: 20,
+                  ),
+                  Center(
+                    child: Text(
+                      _email!,
+                      style:
+                          TextStyle(fontSize: 18, color: ColourTheme.lightGrey),
                     ),
-                    Center(
-                      child: Text(
-                        _name!,
-                        style:
-                            TextStyle(fontSize: 22, color: ColourTheme.white),
-                      ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.06,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: const Text(
+                      'MOBILE NUMBER',
+                      style: TextStyle(fontSize: 18, color: Color(0xFF93959E)),
                     ),
-                    Center(
-                      child: Text(
-                        _email!,
-                        style: TextStyle(
-                            fontSize: 18, color: ColourTheme.lightGrey),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Text(
-                        'MOBILE NUMBER',
-                        style:
-                            TextStyle(fontSize: 18, color: ColourTheme.white),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Container(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0, top: 5),
+                    child: Container(
                       padding: const EdgeInsets.only(left: 20),
                       child: Text(
                         _phone!,
                         style:
-                            TextStyle(fontSize: 18, color: ColourTheme.white),
+                            TextStyle(fontSize: 24, color: ColourTheme.white),
                       ),
                     ),
-                    const SizedBox(
-                      height: 30,
+                  ),
+                  Divider(
+                    color: const Color(0xFF101010),
+                    thickness: 4.0,
+                    indent: MediaQuery.of(context).size.width * 0.04,
+                    endIndent: MediaQuery.of(context).size.width * 0.04,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 20, top: 10),
+                    child: const Text(
+                      'YEAR',
+                      style: TextStyle(fontSize: 18, color: Color(0xFF93959E)),
                     ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Text(
-                        'YEAR',
-                        style:
-                            TextStyle(fontSize: 18, color: ColourTheme.white),
-                      ),
+                  ),
+                  Container(
+                    padding:
+                        const EdgeInsets.only(left: 20, bottom: 10.0, top: 5),
+                    child: Text(
+                      (_year == "one")
+                          ? "1st Year"
+                          : (_year == "two")
+                              ? "2nd Year"
+                              : (_year == "three")
+                                  ? "3rd Year"
+                                  : (_year == "four")
+                                      ? "4th Year"
+                                      : "None",
+                      style: TextStyle(fontSize: 24, color: ColourTheme.white),
                     ),
-                    const SizedBox(
-                      height: 5,
+                  ),
+                  Divider(
+                    color: const Color(0xFF101010),
+                    thickness: 4.0,
+                    indent: MediaQuery.of(context).size.width * 0.04,
+                    endIndent: MediaQuery.of(context).size.width * 0.04,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                      top: 10,
                     ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Text(
-                        (_year == "one")
-                            ? "1st Year"
-                            : (_year == "two")
-                                ? "2nd Year"
-                                : (_year == "three")
-                                    ? "3rd Year"
-                                    : (_year == "four")
-                                        ? "4th Year"
-                                        : "None",
-                        style:
-                            TextStyle(fontSize: 18, color: ColourTheme.white),
-                      ),
+                    child: const Text(
+                      'COLLEGE',
+                      style: TextStyle(fontSize: 18, color: Color(0xFF93959E)),
                     ),
-                    const SizedBox(
-                      height: 30,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(
+                      left: 20,
+                      bottom: 10.0,
+                      top: 5,
                     ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Text(
-                        'COLLEGE',
-                        style:
-                            TextStyle(fontSize: 18, color: ColourTheme.white),
-                      ),
+                    child: Text(
+                      (_college == null) ? "None" : _college!,
+                      style: TextStyle(fontSize: 24, color: ColourTheme.white),
                     ),
-                    const SizedBox(
-                      height: 5,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.only(left: 20),
-                      child: Text(
-                        (_college == null) ? "None" : _college!,
-                        style:
-                            TextStyle(fontSize: 18, color: ColourTheme.white),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: Align(
-                          alignment: FractionalOffset.bottomCenter,
-                          child: TextButton.icon(
-                            onPressed: () async {
-                              try {
-                                await supa.signOut();
-                              } on AuthException catch (error) {
-                                debugPrint(error.toString());
-                              }
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      child: TextButton(
+                        onPressed: () async {
+                          try {
+                            await supa.signOut();
+                          } on AuthException catch (error) {
+                            debugPrint(error.toString());
+                          }
 
-                              if (mounted) {
-                                Navigator.pushAndRemoveUntil(
-                                  context,
-                                  CupertinoPageRoute(
-                                      builder: (context) => const Login()),
-                                  (Route<dynamic> route) => false,
-                                );
-                              }
-                            },
-                            icon: Icon(
-                              Icons.logout,
-                              color: ColourTheme.white,
+                          if (mounted) {
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              CupertinoPageRoute(
+                                  builder: (context) => const Login()),
+                              (Route<dynamic> route) => false,
+                            );
+                          }
+                        },
+                        style: ButtonStyle(
+                          shape:
+                              MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(25),
                             ),
-                            label: Text(
-                              'Log out',
-                              style: TextStyle(color: ColourTheme.white),
-                            ),
+                          ),
+                          backgroundColor: MaterialStateProperty.all<Color>(
+                            const Color(0xFF191919),
+                          ),
+                          foregroundColor: MaterialStateProperty.all<Color>(
+                              ColourTheme.white),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 6),
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.05,
+                              ),
+                              const Icon(
+                                  FontAwesomeIcons.arrowRightFromBracket),
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.03,
+                              ),
+                              const Text(
+                                "Log Out",
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ],
                           ),
                         ),
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                ],
               ),
             ),
     );
