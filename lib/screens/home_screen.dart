bf1134c84ae18c6f7b23c602e9c35f1564b3c11c
Deepraj Baiyda <deeprajbaidya@gmail.com>
2022-11-08 05:25:52 +0530

    fix: Event UI Screen padding and responsive-ness closes #105

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import "package:flutter/material.dart";
 import 'package:youthopia_2022_app/screens/team_reg_form_screen.dart';
 import 'package:youthopia_2022_app/services/events.dart';
-import 'package:intl/intl.dart';
 import 'package:youthopia_2022_app/services/users.dart';
 import '../constants/color_theme.dart';
 import 'individual_reg_form_screen.dart';
@@ -41,16 +40,27 @@ class _HomeState extends State<Home> {
               fontWeight: FontWeight.bold),
         ),
       ),
-      body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 18),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          top: 8.0,
+        ),
         child: ListView(
           children: <Widget>[
             Stack(
               children: [
-                Image(
-                    image: NetworkImage(widget.event.eventPosterUrl),
-                    width: double.maxFinite,
-                    fit: BoxFit.cover),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.95,
+                      height: MediaQuery.of(context).size.height * 0.35,
+                      child: Image(
+                          image: NetworkImage(widget.event.eventPosterUrl),
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                ),
                 Positioned(
                   top: 00,
                   right: 0,
@@ -69,116 +79,134 @@ class _HomeState extends State<Home> {
             const SizedBox(
               height: 20,
             ),
-            Text(
-              'Event info',
-              style: TextStyle(
-                  color: ColourTheme.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text(
+                'Event info',
+                style: TextStyle(
+                    color: ColourTheme.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22),
+              ),
             ),
             const SizedBox(
               height: 20,
             ),
-            Row(
-              children: [
-                Icon(
-                  Icons.schedule,
-                  size: 25,
-                  color: ColourTheme.white,
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                Text(
-                  widget.event.eventTime,
-                  style: TextStyle(color: ColourTheme.white, fontSize: 20),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.event_available_outlined,
-                  size: 25,
-                  color: ColourTheme.white,
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                Text(
-                  widget.event.eventDate,
-                  style: TextStyle(color: ColourTheme.white, fontSize: 20),
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.location_on_outlined,
-                  size: 25,
-                  color: ColourTheme.white,
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                Flexible(
-                  child: Text(
-                    widget.event.eventVenue,
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.schedule,
+                    size: 25,
+                    color: ColourTheme.white,
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Text(
+                    widget.event.eventTime,
                     style: TextStyle(color: ColourTheme.white, fontSize: 20),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
             const SizedBox(
               height: 10,
             ),
-            Row(
-              children: [
-                Icon(
-                  Icons.currency_rupee,
-                  color: ColourTheme.white,
-                  size: 25,
-                ),
-                const SizedBox(
-                  width: 15,
-                ),
-                Flexible(
-                  child: Text(
-                    (isDIT)
-                        ? widget.event.eventFeesDIT
-                        : widget.event.eventFeesOutsider,
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.event_available_outlined,
+                    size: 25,
+                    color: ColourTheme.white,
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Text(
+                    widget.event.eventDate,
                     style: TextStyle(color: ColourTheme.white, fontSize: 20),
                   ),
-                ),
-              ],
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.location_on_outlined,
+                    size: 25,
+                    color: ColourTheme.white,
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Flexible(
+                    child: Text(
+                      widget.event.eventVenue,
+                      style: TextStyle(color: ColourTheme.white, fontSize: 20),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.currency_rupee,
+                    color: ColourTheme.white,
+                    size: 25,
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  ),
+                  Flexible(
+                    child: Text(
+                      (isDIT)
+                          ? widget.event.eventFeesDIT
+                          : widget.event.eventFeesOutsider,
+                      style: TextStyle(color: ColourTheme.white, fontSize: 20),
+                    ),
+                  ),
+                ],
+              ),
             ),
             (widget.event.isTeamEvent)
-                ? Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        'Minimum members: ${widget.event.eventMinMembers}',
-                        style:
-                            TextStyle(color: ColourTheme.white, fontSize: 20),
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Maximum members: ${widget.event.eventMaxMembers}',
-                        style:
-                            TextStyle(color: ColourTheme.white, fontSize: 20),
-                      ),
-                    ],
+                ? Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Minimum members: ${widget.event.eventMinMembers}',
+                          style:
+                              TextStyle(color: ColourTheme.white, fontSize: 20),
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Maximum members: ${widget.event.eventMaxMembers}',
+                          style:
+                              TextStyle(color: ColourTheme.white, fontSize: 20),
+                        ),
+                      ],
+                    ),
                   )
                 : const SizedBox(
                     height: 0,
@@ -186,63 +214,73 @@ class _HomeState extends State<Home> {
             const SizedBox(
               height: 40,
             ),
-            Text(
-              'Description',
-              style: TextStyle(
-                  color: ColourTheme.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text(
+                'Description',
+                style: TextStyle(
+                    color: ColourTheme.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20),
+              ),
             ),
             const SizedBox(
               height: 20,
             ),
-            Text(
-              widget.event.eventDescription,
-              style: TextStyle(color: ColourTheme.white, fontSize: 18),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0),
+              child: Text(
+                widget.event.eventDescription,
+                style: TextStyle(color: ColourTheme.white, fontSize: 18),
+              ),
             ),
             const SizedBox(
               height: 20,
             ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: 50.0,
-              decoration: BoxDecoration(
-                  gradient: ColourTheme.buttonGradient,
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(0.0, 1.5),
-                      blurRadius: 1.5,
-                    ),
-                  ],
-                  borderRadius: BorderRadius.circular(50)),
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                    onTap: () {
-                      (!widget.event.isTeamEvent)
-                          ? Navigator.push(
-                              context,
-                              CupertinoPageRoute(
-                                  builder: (context) =>
-                                      DITIndividualRegFormScreen(widget.event)))
-                          : Navigator.push(
-                              context,
-                              CupertinoPageRoute(
-                                  builder: (context) =>
-                                      DITTeamRegFormScreen(widget.event)));
-                    },
-                    child: const Center(
-                        child: Padding(
-                      padding: EdgeInsets.symmetric(vertical: 6),
-                      child: Text(
-                        "Register for this Event",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                            color: Colors.white),
+            Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15),
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: 50.0,
+                decoration: BoxDecoration(
+                    gradient: ColourTheme.buttonGradient,
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 1.5),
+                        blurRadius: 1.5,
                       ),
-                    ))),
+                    ],
+                    borderRadius: BorderRadius.circular(50)),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                      onTap: () {
+                        (!widget.event.isTeamEvent)
+                            ? Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) =>
+                                        DITIndividualRegFormScreen(
+                                            widget.event)))
+                            : Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                    builder: (context) =>
+                                        DITTeamRegFormScreen(widget.event)));
+                      },
+                      child: const Center(
+                          child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 6),
+                        child: Text(
+                          "Register for this Event",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.white),
+                        ),
+                      ))),
+                ),
               ),
             ),
             const SizedBox(
