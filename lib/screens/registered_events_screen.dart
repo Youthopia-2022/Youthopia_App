6906c73486ed25425d1ae9c7962bb0cd9f8d9dfd
Deepraj Baiyda <deeprajbaidya@gmail.com>
2022-11-08 05:58:37 +0530

    fix: registered events Components

--- lib/screens/registered_events_screen.dart
+++ lib/screens/registered_events_screen.dart
@@ -155,74 +155,93 @@ class _RegisteredEventsState extends State<RegisteredEvents> {
                         SizedBox(
                           height: MediaQuery.of(context).size.height * 0.01,
                         ),
-                        Container(
-                          color: const Color(0xFF1B1B1B),
-                          padding: const EdgeInsets.symmetric(horizontal: 15),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              SizedBox(
-                                width: MediaQuery.of(context).size.width * 0.4,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    SizedBox(
-                                      height:
-                                          MediaQuery.of(context).size.height *
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0, right: 8),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              height: MediaQuery.of(context).size.height * 0.2,
+                              color: const Color(0xFF1B1B1B),
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 15),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width * 0.4,
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        SizedBox(
+                                          height: MediaQuery.of(context)
+                                                  .size
+                                                  .height *
                                               0.01,
-                                    ),
-                                    Text(
-                                      RegisteredEvent
-                                          .registeredEvents[index].eventName,
-                                      style: TextStyle(
-                                          fontSize: 22,
-                                          color: ColourTheme.white),
-                                    ),
-                                    Padding(
-                                      padding: const EdgeInsets.only(
-                                          bottom: 13.0, top: 10),
-                                      child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.start,
-                                        children: [
-                                          const Icon(FontAwesomeIcons.clock,
-                                              size: 15,
-                                              color: Color(0xFF666666)),
-                                          Text(
-                                            '   ${eventTime}',
-                                            style: TextStyle(
-                                                fontSize: 14,
-                                                color: ColourTheme.lightGrey),
+                                        ),
+                                        Text(
+                                          RegisteredEvent
+                                              .registeredEvents[index]
+                                              .eventName,
+                                          style: TextStyle(
+                                              fontSize: 22,
+                                              color: ColourTheme.white),
+                                        ),
+                                        Padding(
+                                          padding: const EdgeInsets.only(
+                                              bottom: 13.0, top: 10),
+                                          child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            children: [
+                                              const Icon(FontAwesomeIcons.clock,
+                                                  size: 15,
+                                                  color: Color(0xFF666666)),
+                                              Text(
+                                                '   $eventTime',
+                                                style: TextStyle(
+                                                    fontSize: 14,
+                                                    color:
+                                                        ColourTheme.lightGrey),
+                                              ),
+                                            ],
                                           ),
-                                        ],
+                                        ),
+                                        Text(
+                                          eventVenue,
+                                          //overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                              fontSize: 17,
+                                              color: ColourTheme.lightGrey),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 8.0, bottom: 8),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(15),
+                                      child: SizedBox(
+                                        height:
+                                            MediaQuery.of(context).size.width *
+                                                0.45,
+                                        child: Image(
+                                          image: NetworkImage(
+                                            RegisteredEvent
+                                                .registeredEvents[index]
+                                                .eventPoster,
+                                          ),
+                                        ),
                                       ),
                                     ),
-                                    Text(
-                                      eventVenue,
-                                      //overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                          fontSize: 17,
-                                          color: ColourTheme.lightGrey),
-                                    )
-                                  ],
-                                ),
+                                  )
+                                ],
                               ),
-                              ClipRRect(
-                                borderRadius:
-                                    const BorderRadius.all(Radius.circular(5)),
-                                child: Image(
-                                  image: NetworkImage(
-                                    RegisteredEvent
-                                        .registeredEvents[index].eventPoster,
-                                  ),
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.5,
-                                  height:
-                                      MediaQuery.of(context).size.width * 0.5,
-                                ),
-                              )
-                            ],
+                            ),
                           ),
                         ),
                       ],
